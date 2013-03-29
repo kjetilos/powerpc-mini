99e2801c2d1e5228f261d4248883954f7271a644
kos <kjetil.osteras@appeartv.com>
2013-03-29 23:06:06 +0100

    Updated kernel and made downloading conditional

--- kernel.sh
+++ kernel.sh
@@ -2,10 +2,21 @@
 
 CWD=$(dirname $(readlink -f $0))
 export PATH=$CWD/tools/compiler/bin:$PATH
-#wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-3.8.4.tar.xz
-#tar xvJf linux-3.8.4.tar.xz
-tar xvjf linux-3.8.2.tar.bz2
+FILE=linux-3.8.5.tar.xz
+DIR=linux-3.8.5
+SYSROOT=$CWD/tree
 
-cd linux-3.8.2
+if [ ! -f $FILE ]
+then
+  wget https://www.kernel.org/pub/linux/kernel/v3.x/$FILE
+fi
+
+if [ ! -d $DIR ]
+then
+  tar xvJf $FILE
+fi
+
+cd $DIR
 make ARCH=powerpc CROSS_COMPILE=powerpc-unknown-linux-gnu- pmac32_defconfig
 make ARCH=powerpc CROSS_COMPILE=powerpc-unknown-linux-gnu- -j 8
+make ARCH=powerpc CROSS_COMPILE=powerpc-unknown-linux-gnu- INSTALL_MOD_PATH="${SYSROOT}" modules_install
--- qemu.sh
+++ qemu.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 CWD=$(dirname $(readlink -f $0))
-echo wget qemu
-#tar xvjf qemu-1.4.0.tar.bz2
-#cd qemu-1.4.0
+FILE=qemu-1.4.0.tar.bz2
+DIR=qemu-1.4.0
+
+if [ ! -f $FILE ]
+then
+  wget http://wiki.qemu-project.org/download/$FILE
+fi
+
+if [ ! -d $DIR ]
+then
+  tar xvjf $FILE
+fi
+
+cd $DIR
 ./configure --prefix=$CWD/tools/emulator --target-list=ppc-softmmu --enable-sdl
 make -j 8 && make install
